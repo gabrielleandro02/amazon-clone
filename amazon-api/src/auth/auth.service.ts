02dd6df48940d812dc2b2fe79f6758377a1a51c4
gabriel.leandro <gabriel.leandro@kbase.com.br>
2024-05-30 15:47:46 -0300

    AMAZON-CLONE: Auth API

---
+++ amazon-api/src/auth/auth.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
+import { NewUserDTO } from 'src/user/dto/new-user.dto';
+import { UserDetails } from 'src/user/user.interface';
+
+@Injectable()
+export class AuthService {
+  constructor(private userService: UserService) {}
+
+  async hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, 12);
+  }
+
+  async register(user: Readonly<NewUserDTO>): Promise<UserDetails | any> {
+    const { name, email, password } = user;
+
+    const existingUser = await this.userService.findByEmail(email);
+
+    if (existingUser) return 'Email takebn';
+
+    const hashedPassword = await this.hashPassword(password);
+
+    const newUser = await this.userService.create(name, email, hashedPassword);
+
+    return await this.userService._getUserDetails(newUser);
+  }
+
+  async doesPasswordMatch(
+    password: string,
+    hashedPassword: string,
+  ): Promise<boolean> {
+    return bcrypt.compare(password, hashedPassword);
+  }
+
+  async validateUser(
+    email: string,
+    password: string,
+  ): Promise<UserDetails | null> {
+    const user = await this.userService.findByEmail(email);
+    const doesUserExist = !!user;
+
+    if (!user) return null;
+
+    const doesPasswordMatch = await this.doesPasswordMatch(password);
+    //TODO
+  }
+}
